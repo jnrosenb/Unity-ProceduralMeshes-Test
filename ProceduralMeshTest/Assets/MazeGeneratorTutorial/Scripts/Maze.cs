484c0458c7f51883840920bed61e04243a765241
DESKTOP-V6SR2VJ\pc3 <devconexiones01@gmail.com>
2017-06-21 16:01:49 -0400

    Update: Generating small random sections of terrain

---
+++ ProceduralMeshTest/Assets/MazeGeneratorTutorial/Scripts/Maze.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour {
+
+	public intVector2 size = new intVector2 (20, 20);
+
+	public MazeCell cellTemplate;
+	private MazeCell[,] mazeGrid;
+
+	public float cellCreationDelay = 0.01f;
+
+	public intVector2 RandomCoordinates
+	{
+		get
+		{
+			return new intVector2((int)Random.Range(0, size.x), (int)Random.Range(0, size.z));
+		}
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	//Metodo que se llama desde GameManager:
+	public void generateMaze()
+	{
+		StartCoroutine (generateCorroutine());
+	}
+
+	//Corroutine only to see the generation process in slow motion.
+	public IEnumerator generateCorroutine()
+	{
+		mazeGrid = new MazeCell[size.z, size.x];
+
+		intVector2 randomCoords = RandomCoordinates;
+		while (containsCoords (randomCoords) && ReferenceEquals(mazeGrid[randomCoords.z, randomCoords.x], null))
+		{
+			createCell (randomCoords);
+
+			randomCoords += mazeDirections.RandomDirection.toIntVector2 ();
+
+			yield return new WaitForSeconds (cellCreationDelay);
+		}
+	}
+
+	//Metodo innecesario, pero por ahora lo usare:
+	private bool containsCoords(intVector2 coord)
+	{
+		return (coord.x >= 0 && coord.x < size.x && coord.z >= 0 && coord.z < size.z);
+	}
+
+	//coords.x corresponde a la j (la columna), coords.z corresponde a la i (la fila):
+	private void createCell(intVector2 coords)
+	{
+		MazeCell cell = GameObject.Instantiate (cellTemplate) as MazeCell;
+		float cellSize = cell.GetComponentInChildren<MeshRenderer> ().bounds.size.x;
+		cell.transform.SetParent (transform);
+		cell.name = "Cell" + coords.z + coords.x;
+
+		cell.transform.localPosition = new Vector3(coords.x * cellSize, 0f, coords.z * cellSize);
+		cell.transform.position -= new Vector3(size.x  * cellSize * 0.5f - cellSize * 0.5f, 0f, size.z * cellSize * 0.5f - cellSize * 0.5f);
+
+		mazeGrid [coords.z, coords.x] = cell;
+	}
+}
---
+++ ProceduralMeshTest/Assets/MazeGeneratorTutorial/Scripts/Structs.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * STRUCT---------------------------------------*** 
+ * struct for representing a 2d vector with ints.
+ *
+*/
+
+[System.Serializable]
+public struct intVector2
+{
+	public int x;
+	public int z;
+
+	public intVector2(int x, int z)
+	{
+		this.x = x;
+		this.z = z;
+	}
+
+	public static intVector2 operator +(intVector2 A, intVector2 B)
+	{
+		return new intVector2 (A.x + B.x, A.z + B.z);
+	}
+
+	public static intVector2 operator -(intVector2 A, intVector2 B)
+	{
+		return new intVector2 (A.x - B.x, A.z - B.z);
+	}
+
+	public static intVector2 operator *(intVector2 A, int B)
+	{
+		return new intVector2 (A.x * B, A.z * B);
+	}
+
+	public static intVector2 operator *(int B, intVector2 A)
+	{
+		return new intVector2 (A.x * B, A.z * B);
+	}
+
+	public static intVector2 operator /(intVector2 A, int B)
+	{
+		return new intVector2 (A.x / B, A.z / B);
+	}
+}
